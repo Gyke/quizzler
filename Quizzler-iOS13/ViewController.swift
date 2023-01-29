f25564fad999a036dab1b98df0212903e483c6bf
Konstantin <don.kamatoz@bk.ru>
2023-01-29 15:10:59 +0500

    Сделал первый челендж анджелы, сел делать второй челендж и увлекся. Теперь приложение спрашивает вопросы из заданного списка, Сверяет их с првильными, изменяет прогрес бар ,а в конце выводит поздравление и счет пользователя. Планирую сделать кнопку трай агейн, но надеюсь у Анжелы будет об этом. Если нет, сам сделаю, по крайней мере постараюсь не забыть.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -8,11 +8,72 @@
 
 import UIKit
 
+class iQuiz {
+    let question : String
+    let answer   : Bool
+    let price    : Int
+    
+    init(question: String, answer: Bool, price: Int = 1) {
+        self.question = question
+        self.answer = answer
+        self.price = price
+    }
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressbarView: UIProgressView!
+    @IBOutlet weak var falseButtonView: UIButton!
+    @IBOutlet weak var trueButtonView: UIButton!
+    @IBOutlet weak var questionLabelView: UILabel!
+    
+    let quiz = [
+        iQuiz(question: "now is 6:82 AM", answer: false),
+        iQuiz(question: "two plus four is equal six", answer: true),
+        iQuiz(question: "you are not a robot", answer: true),
+    ]
+    
+    var currentScore = 0
+    var attemptCount = 0
+    var maxAttempt = 0
+    
+    var currentAnswer = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        maxAttempt = quiz.count
+        setQuiz()
+    }
+    
+    func setQuiz() {
+        progressbarView.progress = Float(attemptCount * 100 / maxAttempt) / 100.0
+        print(Float(attemptCount / maxAttempt))
+        if attemptCount == maxAttempt {
+            trueButtonView.isHidden = true
+            falseButtonView.isHidden = true
+            progressbarView.isHidden = true
+            
+            questionLabelView.text = "Congratulations!\nYour score: \(self.currentScore)"
+            return
+        }
+        let tmp = quiz[self.attemptCount]
+        questionLabelView.text = tmp.question
+        self.currentAnswer = tmp.answer
+    }
+    
+    func checkAnswer(answer: Bool) -> Bool {
+        if self.currentAnswer == answer {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if checkAnswer(answer: sender.currentTitle! == "True") {
+            currentScore += 1
+        }
+        attemptCount += 1
+        setQuiz()
     }
 
 
